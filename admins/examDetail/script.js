1f3d7d74328b7151ef83eef09e1d2b3b26abd73e
Zlash01 <xhunterx2003@gmail.com>
2024-05-14 01:07:32 +0700

    done create

--- admins/examDetail/script.js
+++ admins/examDetail/script.js
@@ -33,63 +33,25 @@ function getDataExams() {
 }
 getDataExams();
 
-//get all questions?
 function getExamQuestions() {
-  const exam_id = localStorage.getItem("exam_id");
-  apiGet(`/api/questions/exam/${exam_id}`, localStorage.getItem("token"))
-    .then((response) => {
-      const questionsWithId = response.map(({ id, question_text }) => ({
-        id,
-        question_text,
-        answers: [], // Initialize answers array for each question
-      }));
-      console.log(questionsWithId);
-      localStorage.setItem("questions", JSON.stringify(questionsWithId));
-    })
-    .catch((error) => {
-      console.error("Error fetching exams:", error);
-    });
+  var exam_id = localStorage.getItem("exam_id");
+  apiGet(
+    `/api/questions/all-question/${exam_id}`,
+    localStorage.getItem("token")
+  ).then((response) => {
+    console.log(response);
+    localStorage.setItem("questions", JSON.stringify(response));
+  });
 }
 getExamQuestions();
 
-// get all answers?
-function getExamAnswers() {
-  const questionsWithId = JSON.parse(localStorage.getItem("questions"));
-  console.log(questionsWithId);
-  const token = localStorage.getItem("token");
-  const promises = questionsWithId.map(({ id, question_text }) => {
-    return apiGet(`/api/answers/${id}`, token)
-      .then((response) => {
-        const answers = response.map(({ id, answer_text, is_correct }) => ({
-          id,
-          answer_text,
-          is_correct,
-        }));
-        const question = questionsWithId.find((q) => q.id === id);
-        question.answers = answers;
-        //console.log(`Answers for question ${id} (${question_text}):`, answers);
-      })
-      .catch((error) => {
-        console.error(
-          `Error fetching answer for question ${id} (${question_text}):`,
-          error
-        );
-      });
-  });
-  Promise.all(promises).then(() => {
-    localStorage.setItem("questions", JSON.stringify(questionsWithId));
-    console.log("All answers added to questions JSON.");
-    console.log(questionsWithId);
-  });
-}
-getExamAnswers();
-
+//get all questions?
 function addQuestionsToList() {
   const jsonData = JSON.parse(localStorage.getItem("questions"));
-
   const questionList = document.getElementById("question-list");
 
-  jsonData.forEach((question, index) => {
+  jsonData.forEach((questionObj, index) => {
+    const question = questionObj.question;
     const questionBox = document.createElement("div");
     questionBox.classList.add("question-box");
 
@@ -105,16 +67,17 @@ function addQuestionsToList() {
 
     question.answers.forEach((answer, answerIndex) => {
       const answerDiv = document.createElement("div");
-
       const inputRadio = document.createElement("input");
       inputRadio.type = "radio";
       inputRadio.id = `q${index + 1}-a${answerIndex + 1}`;
       inputRadio.name = `q${index + 1}`;
       inputRadio.value = answer.answer_text;
       inputRadio.disabled = true; // Disable the radio buttons
+
       if (answer.is_correct) {
         inputRadio.checked = true;
       }
+
       answerDiv.appendChild(inputRadio);
 
       const labelRadio = document.createElement("label");
@@ -128,6 +91,7 @@ function addQuestionsToList() {
     questionList.appendChild(questionBox);
   });
 }
+
 addQuestionsToList();
 
 function logout() {
